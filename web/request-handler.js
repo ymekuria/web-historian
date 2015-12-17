b72edce0b914fde761e5a8c5ee0047e496cac5f2
ymekuria <ymekuria@gmail.com>
2015-12-17 11:54:58 -0800

    "used "isInURL" helper to return a 404 status for nonexisting files"

--- web/request-handler.js
+++ web/request-handler.js
@@ -3,27 +3,45 @@ var archive = require('../helpers/archive-helpers');
 var httpHelpers = require('./http-helpers.js');
 
 exports.handleRequest = function (req, res) {
- console.log('req',req.url);
+  var isURL = archive.isUrlArchived(req.url, function() {console.log(true); });
+ console.log('isUrlInList:');
+ console.log('req:',req.url);
+  
 
   if( req.method === 'GET') {
-    res.writeHead(200,httpHelpers.headers);
+    
 
     if ( req.url === '/') {
       httpHelpers.serveAssets(res,'index.html',function(data) {
+        res.writeHead(200,httpHelpers.headers);
         res.end(data);
-      }); // if url is in the archive
-    } else if ( httpHelpers.isUrlArchived(req.url, function(isArchived) {
-      console.log(isArchived); }) ) {
-      // do something
+      }); 
+    } else if ( !isURL ) {
+      res.writeHead(404,httpHelpers.headers);
+      res.end();
 
-      //if not do 404
-    } else if ( !httpHelpers.isUrlArchived(req.url, function(isArchived) {
-      console.log(isArchived); }) ) {
+    } else if ( isURL ) {
+      //magic happens
+      res.writeHead(200, httpHelpers.headers);
+      //get into sites/google folder
+      fs.readFile('path--file in', function(error,data){
+        if (error) {
+          throw error;
+        } else {
+          res.end(data);
+        }
+      })
+
+        //read contents of the google file in here
+          //return contents
+
+    }
+
   }
-
-
 };
 
+// !httpHelpers.isUrlInList(req.url, function() {
+//       console.log(true); })
 // var getOptions = function(getRequest, res, asset) {
 //   httpHelpers[getRequest](res, asset, function(data) {
 //     res.end(data);
